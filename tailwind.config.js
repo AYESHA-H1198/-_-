8c16ad721abd2a4d46aded832406b52e5676243d
Ayesha Fayyaz <160034982+AYESHA-H1198@users.noreply.github.com>
2024-08-15 08:14:02 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: '620px',
+      md: '748px',
+      lg: '950px',
+      xl: '1190px',
+    },
+    fontFamily: {
+      primary : 'var(--font-jetbrainsMono)',
+    },
+    extend: {
+      colors: {
+        primary: '#5B2F47',
+        accent : {
+          DEFAULT : "#F5CEC7",
+          hover: "#B96755",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
